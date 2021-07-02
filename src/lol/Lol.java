8b438ff0bd3299d5cdcf6f387477140059b01387
Matias <Matias@matias>
2021-07-02 13:37:26 -0300

    prubas_de_Commit

---
+++ src/lol/Lol.java
@@ -0,0 +1,12 @@
+package lol;
+
+public class Lol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("lol");
+    }
+    
+}
