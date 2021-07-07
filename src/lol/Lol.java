2bbda6a07bc51d2324bbcf8648c4294971479f7d
Matias <Matias@matias>
2021-07-07 12:02:23 -0300

    pruva2.1

--- src/lol/Lol.java
+++ src/lol/Lol.java
@@ -1,12 +1,30 @@
 package lol;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Lol {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("lol");
+        Scanner dato = new Scanner(System.in);
+        
+        System.out.println("Ingrese un numero");
+        try{
+        int num = dato.nextInt();
+        System.out.println("lol: " + num);
+        }catch(InputMismatchException ex){
+            System.out.println("Eerr: " +ex.getMessage());        
+        }catch(Exception e){
+        System.out.println("Eerr: " +e.getMessage());
+        }finally{
+        System.out.println("Ingrese un numero");
+        int num2 = dato.nextInt();
+        System.out.println("lol: " + num2);
+        }
     }
     
 }
