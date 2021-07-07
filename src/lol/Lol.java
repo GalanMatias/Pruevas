731a3bf126d4c6cbb61e28f90a4f13a0ceee74e8
Matias <Matias@matias>
2021-07-07 12:04:16 -0300

    pruva2.2

--- src/lol/Lol.java
+++ src/lol/Lol.java
@@ -10,21 +10,22 @@ public class Lol {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner dato = new Scanner(System.in);
-        
-        System.out.println("Ingrese un numero");
-        try{
-        int num = dato.nextInt();
-        System.out.println("lol: " + num);
-        }catch(InputMismatchException ex){
-            System.out.println("Eerr: " +ex.getMessage());        
-        }catch(Exception e){
-        System.out.println("Eerr: " +e.getMessage());
-        }finally{
-        System.out.println("Ingrese un numero");
-        int num2 = dato.nextInt();
-        System.out.println("lol: " + num2);
-        }
+//        Scanner dato = new Scanner(System.in);
+//        
+//        System.out.println("Ingrese un numero");
+//        try{
+//        int num = dato.nextInt();
+//        System.out.println("lol: " + num);
+//        }catch(InputMismatchException ex){
+//            System.out.println("Eerr: " +ex.getMessage());        
+//        }catch(Exception e){
+//        System.out.println("Eerr: " +e.getMessage());
+//        }finally{
+//        System.out.println("Ingrese un numero");
+//        int num2 = dato.nextInt();
+//        System.out.println("lol: " + num2);
+//        }
+System.out.println("Ingrese un numero");
     }
     
 }
